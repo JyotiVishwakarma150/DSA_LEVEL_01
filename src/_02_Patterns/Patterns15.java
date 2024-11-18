a51094190540dd67c72ba89c89631f0ebec3af32
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2024-11-18 20:08:35 +0530

    Patterns15

--- src/_02_Patterns/Patterns15.java
+++ src/_02_Patterns/Patterns15.java
@@ -6,19 +6,28 @@ public class Patterns15 {
       int n = scn.nextInt();
         int sp=n/2;
         int st=1;
+        int val =1;
         for(int i=1;i<=n;i++){
             for (int j=1;j<=sp;j++){
                 System.out.print("\t");
             }
+            int cval = val;
             for(int j=1;j<=st;j++){
-                System.out.print(st + "\t");
+                System.out.print(cval + "\t");
+                if(j<=st/2){
+                    cval++;
+                } else{
+                    cval--;
+                }
             }
-            if(i<=n){
+            if(i<=n/2){
                 sp--;
-                st++;
+                st += 2;
+                val++;
             } else{
                 sp++;
-                st--;
+                st-=2;
+                val--;
 
             }
             System.out.println();
