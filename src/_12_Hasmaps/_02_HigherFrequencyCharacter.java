8580cb4c076f32fe5c83f0f32a9d9cd5ffaf45d1
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-04-03 22:36:26 +0530

    HigherFrequencyCharacter

---
+++ src/_12_Hasmaps/_02_HigherFrequencyCharacter.java
@@ -0,0 +1,30 @@
+package _12_Hasmaps;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class _02_HigherFrequencyCharacter {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        HashMap<Character,Integer> hm = new HashMap<>();
+        for(int i=0;i<str.length();i++){ //Loop chala rahe hain poori string par, har ek character ka count increase kar rahe hain.
+            char ch = str.charAt(i);
+            if(hm.containsKey(ch)){
+                int of = hm.get(ch); // of = old frequency
+                int nf = of+1; // nf=new frequency.... Agar character pehle se hai, toh uski frequency 1 se badha dete hain
+                hm.put(ch,nf);
+            }else{ //Agar character pehli baar aaya hai, toh 1 set kar dete hain
+                hm.put(ch,1);
+            }
+        }
+        //mfc variable banaya jo sabse zyada repeat hone wale character ko store karega.
+        char mfc = str.charAt(0); // mfc = max frequency character
+        for(Character key : hm.keySet()){//Loop chalaya har ek character ke liye jo hashmap me hai.
+            if(hm.get(key)>hm.get(mfc)){//Jo bhi character ki frequency maximum hai, usko update kar diya
+                mfc = key;
+            }
+        }
+        System.out.println(mfc);
+    }
+}
