67a234e83132708e55a1b4dadeaad7f43aea42b3
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-04-09 00:16:21 +0530

    WritePriorityQueueUsingHeap

--- src/_12_HashmapsAndHeap/_11_WritePriorityQueueUsingHeap.java
+++ src/_12_HashmapsAndHeap/_11_WritePriorityQueueUsingHeap.java
@@ -5,23 +5,23 @@ public class _11_WritePriorityQueueUsingHeap {
     public static class PriorityQueue {
         ArrayList<Integer> data;
 
-        public PriorityQueue() {
+        public PriorityQueue() {//Jab queue create hoti hai, to empty ArrayList ban jati hai.
             data = new ArrayList<>();
         }
 
         public void add(int val) {
             // write your code here
-            data.add(val);
-            upheapify(data.size()-1);
+            data.add(val);//Value ko list ke end mein daal diya.
+            upheapify(data.size()-1);//Fir upheapify call kiya, jisse value ko correct position pe laaya ja sake heap ke rules ke according
         }
         private void upheapify(int i){
             if(i==0){
                 return;
             }
-            int pi=(i-1)/2;
-            if(data.get(i)<data.get(pi)){
+            int pi=(i-1)/2;//Har child ka parent i-1/2 hota hai.
+            if(data.get(i)<data.get(pi)){//Agar child < parent, to swap kar do.
                 swap(i,pi);
-                upheapify(pi);
+                upheapify(pi);//Yeh recursively hota hai jab tak root node ya correct position nahi mil jata.
             }
         }
         private void swap(int i,int j){
@@ -37,24 +37,24 @@ public class _11_WritePriorityQueueUsingHeap {
                 System.out.println("Underflow");
                 return -1;
             }
-            swap(0,data.size()-1);
-            int val=data.remove(data.size()-1);
-            downheapify(0);
+            swap(0,data.size()-1);//Root (minimum element) ko last element ke saath swap kar diya
+            int val=data.remove(data.size()-1);//Ab last element (jo actual root tha) ko remove kar diya.
+            downheapify(0);//Ab jo element root pe aaya hai (last se utha ke), usko sahi position pe bhejna padega
             return val;
         }
         private void downheapify(int pi){
-            int mini = pi;
-            int li = 2*pi+1;
+            int mini = pi; // assume current is smallest
+            int li = 2*pi+1;// left child index
             if(li<data.size()&&data.get(li)<data.get(mini)){
                 mini=li;
             }
-            int ri=2*pi+2;
+            int ri=2*pi+2; // right child index
             if(ri<data.size()&&data.get(ri)<data.get(mini)){
                 mini=ri;
             }
             if(mini != pi){
-                swap(pi,mini);
-                downheapify(mini);
+                swap(pi,mini);//Agar koi child parent se chhota hai to uske saath swap karo
+                downheapify(mini);//Fir jis child se swap kiya, uske liye bhi downheapify() call karo (recursion)
             }
         }
 
